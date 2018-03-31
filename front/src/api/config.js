155aa0f45c5a443f6f43b1f3260c40a51a27bb6b
jensen5201 <jensen5201@163.com>
2018-03-31 21:35:44 +0800

    - edit elementui

--- front/src/api/config.js
+++ front/src/api/config.js
@@ -2,7 +2,7 @@
 * @Author: jensen
 * @Date:   2018-03-30 10:09:51
 * @Last Modified by:   jensen
-* @Last Modified time: 2018-03-31 02:39:48
+* @Last Modified time: 2018-03-31 18:58:41
 */
 
 
@@ -11,10 +11,20 @@ import Promise from 'es6-promise'
 import { CONST_HEADER }  from './common'
 
 
+// get env  and set env ip
+const isPro = process.env.NODE_ENV == 'production'
+const ip = {
+  proUrl: '//192.168.0.106:8000/api/v1',
+  devUrl: '//192.168.0.106:8000/api/v1',
+}
+
+const baseUrl = isPro ? ip.proUrl : ip.devUrl
+
+
 // config base
 axios.create({
-	// baseUrl: '/',
-	// timeout: 3*1000
+	baseUrl,
+	timeout: isPro? 3*1000 : 10*1000
 })
 
 // cross domain request  should carry cookie
@@ -25,8 +35,7 @@ axios
 	.interceptors
 	.request
 	.use( config => {
-		// config.headers['Content-Type']: 'application/json'
-		// Object.assign(config.headers, CONST_HEADER())
+		Object.assign(config.headers, {'Content-Type': 'application/json;charset=UTF-8'}, CONST_HEADER())
 		return config
 	}, error => {
 		return Promise.inject(error)
--- front/src/views/ensure/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div>
-		<p>按需加载模块</p>
-	</div>
-</template>

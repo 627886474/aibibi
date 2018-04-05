7dec93c5ad3201861ff8aeaaf4a8507ff4495c96
jensen5201 <jensen5201@163.com>
2018-04-05 13:24:41 +0800

    - add login page

---
+++ front/src/views/user/login.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+  div
+    header logo
+    content
+      div
+        section
+          label 用户登录
+        section
+          div
+            label 账号
+            el-input
+          div
+            label 密码
+            el-input
+        section
+          el-button 登录
+        section
+          span 忘记密码？
+</template>
+<script type="text/javascript">
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+<style lang='scss'>
+
+</style>
